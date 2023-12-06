e5b9149e76989fa6b3faf56826c9b294b5b64d13
26june <gj.abordo1@gmail.com>
2023-12-06 11:11:27 +0000

    complete day 2 part 1

--- day2/index.js
+++ day2/index.js
@@ -1,4 +1,29 @@
-const fs = require('fs');
-const coordinateArray = fs.readFileSync('input1.txt').toString().split("\n");
+const fs = require("fs");
+const input = fs.readFileSync("input").toString().split("\n");
 
+let total = 0;
 
+const maximum = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+const mappedInput = input.map((game) => {
+  const gameInfo = game.split(/:|;/);
+  const gameId = +gameInfo[0].split(" ")[1];
+
+  for (let index = 1; index < gameInfo.length; index++) {
+    const splitGameInfo = gameInfo[index].match(/\d+|red|green|blue/g);
+    for (let index = 0; index < splitGameInfo.length; index += 2) {
+      const diceNumber = splitGameInfo[index];
+      const diceColour = splitGameInfo[index + 1];
+      if (diceNumber > maximum[diceColour]) {
+        return;
+      }
+    }
+  }
+  total += gameId;
+});
+
+console.log(total);
