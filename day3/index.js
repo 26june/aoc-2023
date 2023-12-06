3b7d97f18a092034c8b835698cf894379f90b89f
26june <gj.abordo1@gmail.com>
2023-12-06 13:32:44 +0000

    day 3 part 1 i dont even know man

--- day3/index.js
+++ day3/index.js
@@ -1,3 +1,47 @@
 const fs = require("fs");
 const input = fs.readFileSync("input.txt").toString().split("\n");
 input.pop();
+
+let total = 0;
+
+input.map((e, i) => {
+  const symbolMatcher = /[^0-9a-zA-Z.]/g;
+  const numberMatcher = /\d+/g;
+  while ((matched = symbolMatcher.exec(e)) != null) {
+    while ((numMatchPrev = numberMatcher.exec(input[i - 1])) != null) {
+      if (
+        numberMatcher.lastIndex - 1 === matched.index - 1 ||
+        numberMatcher.lastIndex - 1 === matched.index ||
+        numberMatcher.lastIndex - 1 === matched.index + 1 ||
+        numMatchPrev.index === matched.index + 1 ||
+        numMatchPrev.index === matched.index
+      ) {
+        total += +numMatchPrev;
+      }
+    }
+    while ((numMatchCurrent = numberMatcher.exec(input[i])) != null) {
+      if (
+        numberMatcher.lastIndex - 1 === matched.index - 1 ||
+        numberMatcher.lastIndex - 1 === matched.index ||
+        numberMatcher.lastIndex - 1 === matched.index + 1 ||
+        numMatchCurrent.index === matched.index + 1 ||
+        numMatchCurrent.index === matched.index
+      ) {
+        total += +numMatchCurrent;
+      }
+    }
+    while ((numMatchNext = numberMatcher.exec(input[i + 1])) != null) {
+      if (
+        numberMatcher.lastIndex - 1 === matched.index - 1 ||
+        numberMatcher.lastIndex - 1 === matched.index ||
+        numberMatcher.lastIndex - 1 === matched.index + 1 ||
+        numMatchNext.index === matched.index ||
+        numMatchNext.index === matched.index + 1
+      ) {
+        total += +numMatchNext;
+      }
+    }
+  }
+});
+
+console.log(total);
